6bc0e72d4a389e4bb4a4ae684232796badb7c50b
kietpt2003 <tuankiet29012003@gmail.com>
2024-02-02 23:09:10 +0700

    create middleware folder for validate

---
+++ src/middleware/authen.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.header('Authorization')
+    const token = authHeader && authHeader.split(' ')[1]
+    if (!token) {
+        return res.status(401).json("Don't have token.") //Unauthorized(ko có token gửi từ client về)
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        console.log(decoded)
+        //Write your validation code here. Example:
+        // req.empID = decoded.empID
+        next()
+    } catch (err) {
+        console.log(err)
+        return res.status(403).json('Invalid token') //forbidden(Invalid token)
+    }
+}
+module.exports = verifyToken
