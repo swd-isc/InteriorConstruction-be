3a1926125604e3b6f2fdef0a608f5af5375be09b
kietpt2003 <tuankiet29012003@gmail.com>
2024-02-07 11:36:00 +0700

    save sample data to db

--- src/services/user-services/userCRUD.js
+++ src/services/user-services/userCRUD.js
@@ -1,28 +1,39 @@
+import Account from '../../models/Account';
 import { accountData } from "../../sample-data/AccountData";
-import Account from '../../models/Account'
-import Client from '../../models/Client'
-import mongoose from "mongoose";
+import Client from '../../models/Client';
 import { clientData } from "../../sample-data/ClientData";
+import Material from '../../models/Material';
+import { materialData } from '../../sample-data/MaterialData';
+import Color from '../../models/Color';
+import { colorData } from '../../sample-data/ColorData';
+import Classification from '../../models/Classification';
+import { classificationData } from '../../sample-data/ClassificationData';
+
+import mongoose from "mongoose";
 
 export const getUserData = async () => {
-    // try {
-    //     const url = process.env.URL_DB;
-    //     await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' }).then(async () => {
-    //         // Iterate over the array of fake accounts and save each to the database
-    //         for (let i = 0; i < clientData.length; i++) {
-    //             const account = new Client(clientData[i]);
-    //             await account.save();
-    //             console.log('ok: ', i);
-    //         }
+    try {
+        const url = process.env.URL_DB;
+        await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' }).then(async () => {
+            // Iterate over the array of fake accounts and save each to the database
+            for (let i = 0; i < classificationData.length; i++) {
+                try {
+                    const account = new Classification(classificationData[i]);
+                    await account.save();
+                    console.log('ok: ', i);
+                } catch (error) {
+                    console.error('client', i, 'error:', error.message);
+                }
+            }
 
-    //         console.log('Fake accounts saved to the database!');
-    //     })
-    // } catch (error) {
-    //     console.error('Error saving fake accounts:', error.message);
-    // } finally {
-    //     // Close the database connection
-    //     mongoose.connection.close();
-    // }
+            console.log('Fake accounts saved to the database!');
+        })
+    } catch (error) {
+        console.error(error.message);
+    } finally {
+        // Close the database connection
+        mongoose.connection.close();
+    }
     return {
         name: "Tuan Kiet",
         age: 21,
