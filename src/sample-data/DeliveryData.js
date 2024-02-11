6b89b1c939e815621b496de994d83dfc0be42928
kietpt2003 <tuankiet29012003@gmail.com>
2024-02-12 00:28:42 +0700

    create delivery and return policy sample data

---
+++ src/sample-data/DeliveryData.js
@@ -0,0 +1,7 @@
+export const deliveryData = [
+    {
+        description: "InteriorConstruction provides door-to-door delivery, assembly and placement services according to your wishes:",
+        noCharge: "FREE delivery within the inner districts of Ho Chi Minh City and Hanoi, applicable for orders worth over 10 million.",
+        surcharge: "For the area of neighboring provinces: Charge a reasonable fee based on the transportation distance.",
+    }
+]
---
+++ src/sample-data/ReturnPolicyData.js
@@ -0,0 +1,23 @@
+export const returnPolicyData = [
+    {
+        headerName: 'Return and exchange policy',
+        headerDescription: "InteriorConstruction provides high-quality products and services. You will no longer be worried because InteriorConstruction is always willing to solve product issues during use.",
+        titleName: "RETURN AND EXCHANGE POLICY",
+        titleDescription: "With the desire to ensure customers' benefits and improve service quality, customers can return or exchange goods for the most comfort and satisfaction at InteriorConstruction.",
+        returnExchangeCases: [
+            "The product cannot be brought into the house (narrow stairs, narrow doors, …)",
+            "Insufficient quantity, insufficient set as specified in the order",
+            "The product is defective or not substandard.",
+        ],
+        nonReturnExchangeCases: [
+            "The products that are used, unclean, old, or damaged.",
+            "Insufficient invoices and vouchers.",
+            "Promotional products.",
+        ],
+        returnProcedure: [
+            "Time allowed to exchange or return goods: Within 3 days from the date of delivery, before the invoice is issued.",
+            "When you have a request to exchange or return goods, please contact the InteriorConstruction showroom where the transaction was made or via hotline: 0388415317.",
+            "InteriorConstruction bears the cost of exchange and return services.",
+        ]
+    }
+]
