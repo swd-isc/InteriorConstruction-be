5a93a253b15dcf4bab155fec62ffe560e3c6b1a4
kietpt2003 <tuankiet29012003@gmail.com>
2024-02-12 00:29:38 +0700

    use color services to create delivery and return policy data

--- src/services/ColorServices.js
+++ src/services/ColorServices.js
@@ -1,6 +1,9 @@
 import Color from '../models/Color';
+import Delivery from '../models/Delivery';
 
 import mongoose from "mongoose";
+import { returnPolicyData } from '../sample-data/ReturnPolicyData';
+import { deliveryData } from '../sample-data/DeliveryData';
 
 export const getColorByPage = async (pageReq) => {
     try {
@@ -44,4 +47,48 @@ export const getColorByPage = async (pageReq) => {
         // Close the database connection
         mongoose.connection.close();
     }
+    // try {
+    //     const url = process.env.URL_DB;
+    //     await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' }).then(async () => {
+    //         // Iterate over the array of fake accounts and save each to the database
+    //         for (let i = 0; i < deliveryData.length; i++) {
+    //             try {
+    //                 const account = new Delivery(deliveryData[i]);
+    //                 await account.save();
+    //                 console.log('ok: ', i);
+    //             } catch (error) {
+    //                 console.error('client', i, 'error:', error.message);
+    //             }
+    //         }
+
+    //         console.log('Fake accounts saved to the database!');
+    //     })
+    // } catch (error) {
+    //     console.error(error.message);
+    // } finally {
+    //     // Close the database connection
+    //     mongoose.connection.close();
+    // }
+
+    // try {
+    //     const url = process.env.URL_DB;
+    //     await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' }).then(async () => {
+    //         // Iterate over the array of fake accounts and save each to the database
+    //         for (let i = 0; i < clientData.length; i++) {
+    //             try {
+    //                 let data = await Client.findOneAndUpdate({ firstName: clientData[i].firstName, lastName: clientData[i].lastName }, { contracts: clientData[i].contracts }).exec();
+    //                 console.log('ok: ', data);
+    //             } catch (error) {
+    //                 console.error('client', i, 'error:', error.message);
+    //             }
+    //         }
+
+    //         console.log('Fake accounts saved to the database!');
+    //     })
+    // } catch (error) {
+    //     console.error(error.message);
+    // } finally {
+    //     // Close the database connection
+    //     mongoose.connection.close();
+    // }
 }
