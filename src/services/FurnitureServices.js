13b357d55e5c679253ed5087f08934bd8266cdd4
kietpt2003 <tuankiet29012003@gmail.com>
2024-02-13 11:39:22 +0700

    create some new get furniture

--- src/services/FurnitureServices.js
+++ src/services/FurnitureServices.js
@@ -3,8 +3,9 @@ import Classification from '../models/Classification';
 
 import mongoose from "mongoose";
 
-export const getFurnitureByPage = async (pageReq) => {
+export const getFurnitureByPage = async (mode, pageReq) => {
     try {
+        let sortAsc = sortByInput(mode);
         const itemsPerPage = 10;
         // Parse query parameters
         const page = parseInt(pageReq) || 1;
@@ -18,7 +19,7 @@ export const getFurnitureByPage = async (pageReq) => {
         const url = process.env.URL_DB;
         await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' });
 
-        currentPageData = await Furniture.find({}).sort({ price: 1 }).populate('colors').populate('materials').populate('delivery').populate('classifications').skip(startIndex).limit(itemsPerPage);
+        currentPageData = await Furniture.find({}).sort({ price: sortAsc }).populate('colors').populate('materials').populate('delivery').populate('classifications').skip(startIndex).limit(itemsPerPage);
         return {
             status: 200,
             data: currentPageData,
@@ -103,19 +104,19 @@ export const getFurnitureByType = async (furType) => {
     }
 }
 
-export const getFurnitureByClassification = async (classificationType) => {
+export const getFurnitureByClassificationByType = async (classificationType) => {
     try {
         let data = [];
 
         // Get the data for the current page
         const url = process.env.URL_DB;
         await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' });
-        const isValid = validateClassification(classificationType);
+        const isValid = validateClassificationType(classificationType);
         if (!isValid) {
             return {
                 status: 400,
                 data: data,
-                messageError: "Type must be 'DEFAULT' or 'CUSTOM'"
+                messageError: "Type must be 'DEFAULT' or 'CUSTOM' and cannot be empty."
             }
         }
         //Mở code này ra chạy thử thì trên mongodb nó có chỉ dùng .type để truy cập cái field thì cách này cũng k dc luôn vì classifications là objectId
@@ -200,6 +201,233 @@ export const getFurnitureByClassification = async (classificationType) => {
     }
 }
 
+export const getFurnitureByClassificationByName = async (classificationName) => {
+    try {
+        let data = [];
+
+        // Get the data for the current page
+        const url = process.env.URL_DB;
+        await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' });
+        if (!classificationName) {
+            return {
+                status: 400,
+                data: data,
+                messageError: "Classification name cannot be empty."
+            }
+        }
+
+        data = await Furniture.aggregate([
+            {
+                $lookup: {
+                    from: 'classification',
+                    localField: 'classifications',
+                    foreignField: '_id',
+                    as: 'classifications',
+                },
+            },
+            {
+                $match: {
+                    'classifications.classificationName': {
+                        $regex: classificationName,
+                        $options: 'i',  // 'i' option makes the match case-insensitive
+                    },
+                },
+            },
+            {
+                $lookup: {
+                    from: 'color',
+                    localField: 'colors',
+                    foreignField: '_id',
+                    as: 'colors',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'material',
+                    localField: 'materials',
+                    foreignField: '_id',
+                    as: 'materials',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'delivery',
+                    localField: 'delivery',
+                    foreignField: '_id',
+                    as: 'delivery',
+                },
+            },
+        ]);
+        return {
+            status: 200,
+            data: data,
+            message: data.length !== 0 ? "OK" : "No data"
+        };
+
+    } catch (error) {
+        console.error(error);
+        return {
+            status: 500,
+            messageError: error,
+        }
+    } finally {
+        // Close the database connection
+        mongoose.connection.close();
+    }
+}
+
+export const getFurnitureByMaterial = async (materialName) => {
+    try {
+        let data = [];
+
+        // Get the data for the current page
+        const url = process.env.URL_DB;
+        await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' });
+
+        if (!materialName) {
+            return {
+                status: 400,
+                data: data,
+                messageError: "Material name cannot be empty."
+            }
+        }
+
+        data = await Furniture.aggregate([
+            {
+                $lookup: {
+                    from: 'classification',
+                    localField: 'classifications',
+                    foreignField: '_id',
+                    as: 'classifications',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'color',
+                    localField: 'colors',
+                    foreignField: '_id',
+                    as: 'colors',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'material',
+                    localField: 'materials',
+                    foreignField: '_id',
+                    as: 'materials',
+                },
+            },
+            {
+                $match: {
+                    'materials.name': {
+                        $regex: materialName,
+                        $options: 'i',  // 'i' option makes the match case-insensitive
+                    },
+                },
+            },
+            {
+                $lookup: {
+                    from: 'delivery',
+                    localField: 'delivery',
+                    foreignField: '_id',
+                    as: 'delivery',
+                },
+            },
+        ]);
+        return {
+            status: 200,
+            data: data,
+            message: data.length !== 0 ? "OK" : "No data"
+        };
+
+    } catch (error) {
+        console.error(error);
+        return {
+            status: 500,
+            messageError: error,
+        }
+    } finally {
+        // Close the database connection
+        mongoose.connection.close();
+    }
+}
+
+export const getFurnitureByColor = async (colorName) => {
+    try {
+        let data = [];
+
+        // Get the data for the current page
+        const url = process.env.URL_DB;
+        await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' });
+
+        if (!colorName) {
+            return {
+                status: 400,
+                data: data,
+                messageError: "Color name cannot be empty."
+            }
+        }
+
+        data = await Furniture.aggregate([
+            {
+                $lookup: {
+                    from: 'classification',
+                    localField: 'classifications',
+                    foreignField: '_id',
+                    as: 'classifications',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'color',
+                    localField: 'colors',
+                    foreignField: '_id',
+                    as: 'colors',
+                },
+            },
+            {
+                $match: {
+                    'colors.name': {
+                        $regex: colorName,
+                        $options: 'i',  // 'i' option makes the match case-insensitive
+                    },
+                },
+            },
+            {
+                $lookup: {
+                    from: 'material',
+                    localField: 'materials',
+                    foreignField: '_id',
+                    as: 'materials',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'delivery',
+                    localField: 'delivery',
+                    foreignField: '_id',
+                    as: 'delivery',
+                },
+            },
+        ]);
+        return {
+            status: 200,
+            data: data,
+            message: data.length !== 0 ? "OK" : "No data"
+        };
+
+    } catch (error) {
+        console.error(error);
+        return {
+            status: 500,
+            messageError: error,
+        }
+    } finally {
+        // Close the database connection
+        mongoose.connection.close();
+    }
+}
+
 function validateType(input) {
     // Convert input to lowercase and compare
     const lowerCaseInput = input.toUpperCase();
@@ -211,8 +439,24 @@ function validateType(input) {
     }
 }
 
+function sortByInput(mode) {
+    if (!mode) { //No input => Ascending
+        return 1;
+    }
+    // Convert input to lowercase and compare
+    const lowerCaseInput = mode.toUpperCase();
 
-function validateClassification(input) {
+    if (lowerCaseInput === "DESC") {
+        return -1;  // Descending
+    } else {
+        return 1; // Ascending
+    }
+}
+
+function validateClassificationType(input) {
+    if (!input) {
+        return false;
+    }
     // Convert input to lowercase and compare
     const lowerCaseInput = input.toUpperCase();
 
