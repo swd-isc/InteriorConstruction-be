6d5505775cf81f46252816dd375e8cec09c66a7a
kietpt2003 <tuankiet29012003@gmail.com>
2024-02-15 23:49:40 +0700

    validate id for some function and create filterSessionService function(just structor, not done yet)

--- src/services/FurnitureServices.js
+++ src/services/FurnitureServices.js
@@ -1,4 +1,7 @@
 import Furniture from '../models/Furniture';
+import Color from '../models/Color';
+import Material from '../models/Material';
+import Classification from '../models/Classification';
 
 import mongoose from "mongoose";
 const ObjectId = mongoose.Types.ObjectId;
@@ -146,18 +149,19 @@ export const getFurnitureByPage = async (mode, pageReq) => {
 }
 
 export const getFurnitureById = async (id) => {
-    // Get the data for the current page
-    const url = process.env.URL_DB;
-    await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' });
-
     try {
-        if (!id) {
+        const idValid = await isIdValid(id, 'furniture');
+        if (!idValid.isValid) {
             return {
-                status: 400,
+                status: idValid.status,
                 data: {},
-                messageError: "Required Id"
+                messageError: idValid.messageError
             }
         }
+
+        const url = process.env.URL_DB;
+        await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' });
+
         const data = await Furniture.findById(id)
             .populate({
                 path: 'colors',
@@ -175,7 +179,6 @@ export const getFurnitureById = async (id) => {
                 path: 'classifications',
                 select: '-_id classificationName',
             })
-        // .populate('classifications');
         if (data) {
             return {
                 status: 200,
@@ -202,13 +205,14 @@ export const getFurnitureById = async (id) => {
 
 export const getFurnitureByClassificationId = async (id, pageReq, mode) => {
     try {
-        let data = [];
+        let data = {};
+        const idValid = await isIdValid(id, 'classification');
 
-        if (!id) {
+        if (!idValid.isValid) {
             return {
-                status: 400,
+                status: idValid.status,
                 data: data,
-                messageError: "Required classification Id."
+                messageError: idValid.messageError
             }
         }
         let sortAsc = sortByInput(mode);
@@ -216,7 +220,6 @@ export const getFurnitureByClassificationId = async (id, pageReq, mode) => {
         const itemsPerPage = 10;
         // Parse query parameters
         const page = parseInt(pageReq) || 1;
-        let currentPageData = [];
 
         // Calculate start and end indices for the current page
         const startIndex = (page - 1) * itemsPerPage;
@@ -231,7 +234,7 @@ export const getFurnitureByClassificationId = async (id, pageReq, mode) => {
         if (pageReq > totalPages) {
             return {
                 status: 400,
-                data: currentPageData,
+                data: data,
                 page: page,
                 totalPages: totalPages,
                 messageError: "Your page is higher than expected"
@@ -362,6 +365,223 @@ export const getFurnitureByClassificationId = async (id, pageReq, mode) => {
         // data = await Furniture.find({ classifications: new ObjectId(id) })
         //     .skip(startIndex).limit(itemsPerPage)
         //     .select('name classifications')
+
+        return {
+            status: 200,
+            data: data[0],
+            message: data.length !== 0 ? "OK" : "No data"
+        };
+
+    } catch (error) {
+        console.error(error);
+        return {
+            status: 500,
+            messageError: error,
+        }
+    } finally {
+        // Close the database connection
+        mongoose.connection.close();
+    }
+}
+
+export const filterSessionService = async (reqData) => {
+    try {
+        let { classificationId, sort_by, colorId, materialId } = reqData;
+        let pageReq = reqData.page;
+        let data = {};
+
+        //Validate classificationId
+        let idValid = await isIdValid(classificationId, 'classification');
+        if (!idValid.isValid) {
+            return {
+                status: idValid.status,
+                data: data,
+                messageError: idValid.messageError
+            }
+        }
+
+        //Validate colorId
+        idValid = await isIdValid(colorId, 'color');
+        if (!idValid.isValid) {
+            return {
+                status: idValid.status,
+                data: data,
+                messageError: idValid.messageError
+            }
+        }
+
+        //Validate materialId
+        idValid = await isIdValid(materialId, 'material');
+        if (!idValid.isValid) {
+            return {
+                status: idValid.status,
+                data: data,
+                messageError: idValid.messageError
+            }
+        }
+
+        let sortAsc = sortByInput(sort_by);
+
+        const itemsPerPage = 10;
+        // Parse query parameters
+        const page = parseInt(pageReq) || 1;
+
+        // Calculate start and end indices for the current page
+        const startIndex = (page - 1) * itemsPerPage;
+
+        if (!classificationId) { //filter with no classificationId
+
+        } else if (!colorId && !materialId) { //filter with just classificationId
+
+        } else if (colorId && !materialId) { //filter with classificationId, colorId
+
+        } else if (!colorId && materialId) { //filter with classificationId, materialId
+
+        } else { //filter with classificationId, colorId and materialId
+
+        }
+
+        // Get the data for the current page
+        const url = process.env.URL_DB;
+        await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' });
+
+        const totalItems = await Furniture.countDocuments();
+        const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+        if (pageReq > totalPages) {
+            return {
+                status: 400,
+                data: data,
+                page: page,
+                totalPages: totalPages,
+                messageError: "Your page is higher than expected"
+            };
+        }
+
+
+
+        data = await Furniture.aggregate([
+            {
+                $lookup: {
+                    from: 'color',
+                    localField: 'colors',
+                    foreignField: '_id',
+                    as: 'colorsData',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'material',
+                    localField: 'materials',
+                    foreignField: '_id',
+                    as: 'materialsData',
+                },
+            },
+            {
+                $lookup: {
+                    from: 'classification',
+                    localField: 'classifications',
+                    foreignField: '_id',
+                    as: 'classificationsData',
+                },
+            },
+            {
+                $unwind: '$classificationsData', // Unwind the classifications array
+            },
+            {
+                $group: {
+                    _id: '$classificationsData._id',
+                    colors: { $push: '$colorsData' },
+                    materials: { $push: '$materialsData' },
+                    furnitures: {
+                        $addToSet: {
+                            _id: '$_id',
+                            name: '$name',
+                            imgURL: '$imgURL',
+                            price: '$price',
+                        },
+                    },
+                },
+            },
+            {
+                $match: {
+                    '_id': new ObjectId(id),
+                },
+            },
+            { // Separate the groups for colors, materials and furnitures
+                $facet: {
+                    colors: [
+                        {
+                            $unwind: '$colors', // Unwind the colors array
+                        },
+                        {
+                            $unwind: '$colors', // Unwind the colors array
+                        },
+                        {
+                            $group: {
+                                _id: '$colors._id',
+                                name: { $first: '$colors.name' },
+                                count: { $sum: 1 },
+                            },
+                        },
+                        {
+                            $sort: {
+                                // Specify the field you want to use for sorting, e.g., '_id'
+                                'name': 1,
+                            },
+                        },
+                    ],
+                    materials: [
+                        {
+                            $unwind: '$materials', // Unwind the materials array
+                        },
+                        {
+                            $unwind: '$materials', // Unwind the materials array
+                        },
+                        {
+                            $group: {
+                                _id: '$materials._id',
+                                name: { $first: '$materials.name' },
+                                count: { $sum: 1 },
+                            },
+                        },
+                        {
+                            $sort: {
+                                // Specify the field you want to use for sorting, e.g., '_id'
+                                'name': 1,
+                            },
+                        },
+                    ],
+                    furnitures: [
+                        {
+                            $unwind: '$furnitures', // Unwind the materials array
+                        },
+                        {
+                            $group: {
+                                _id: '$furnitures._id',
+                                name: { $first: '$furnitures.name' },
+                                imgURL: { $first: '$furnitures.imgURL' },
+                                price: { $first: '$furnitures.price' },
+                            },
+                        },
+                        {
+                            $sort: {
+                                // Specify the field you want to use for sorting, e.g., '_id'
+                                'price': sortAsc,
+                            },
+                        },
+                    ],
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    furnitures: { $slice: ['$furnitures', startIndex, itemsPerPage] },
+                    materials: 1,
+                    colors: 1,
+                },
+            },
+        ]);
 
         return {
             status: 200,
@@ -752,5 +972,76 @@ function validateClassificationType(input) {
         return true;  // Input is valid
     } else {
         return false; // Input is not valid
+    }
+}
+
+async function isIdValid(id, model) {
+    if (id === null || id === undefined) {
+        return {
+            status: 400,
+            isValid: false,
+            messageError: 'ObjectId required.'
+        }
+    }
+    try {
+        const url = process.env.URL_DB;
+        await mongoose.connect(url, { family: 4, dbName: 'interiorConstruction' });
+
+        const isValidObjectId = mongoose.Types.ObjectId.isValid(id);
+
+        if (!isValidObjectId) {
+            // The provided id is not a valid ObjectId
+            return {
+                status: 400,
+                isValid: false,
+                messageError: 'Not a valid ObjectId.'
+            }
+        }
+
+        let data = null;
+
+        switch (model) {
+            case 'color':
+                // Check if the color with the given ObjectId exists in the database
+                data = await Color.findById(id);
+                break;
+            case 'material':
+                // Check if the material with the given ObjectId exists in the database
+                data = await Material.findById(id);
+                break;
+            case 'classification':
+                // Check if the classification with the given ObjectId exists in the database
+                data = await Classification.findById(id);
+                break;
+            case 'furniture':
+                // Check if the classification with the given ObjectId exists in the database
+                data = await Furniture.findById(id);
+                break;
+            default:
+                break;
+        }
+
+        if (data !== null) {
+            return {
+                isValid: true,
+            }
+        } else {
+            return {
+                status: 400,
+                isValid: false,
+                messageError: 'ObjectId not found.'
+            }
+        }
+        return data !== null; // Returns true if data exists, false otherwise
+    } catch (error) {
+        console.error('Error checking ObjectId:', error);
+        return {
+            status: 500,
+            isValid: false,
+            messageError: error
+        }
+    } finally {
+        // Close the database connection
+        mongoose.connection.close();
     }
 }
