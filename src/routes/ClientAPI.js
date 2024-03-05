7ac824e9d37d7e26010235c001819d15931fa5cc
kietpt2003 <tuankiet29012003@gmail.com>
2024-03-05 23:18:15 +0700

    client sưagger

--- src/routes/ClientAPI.js
+++ src/routes/ClientAPI.js
@@ -3,8 +3,123 @@ import { clientService } from "../controller/ClientController";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          Client:
+ *            type: object
+ *            properties:
+ *              firstName:
+ *                type: string
+ *              lastName:
+ *                type: string
+ *              birthDate:
+ *                type: string
+ *              phone:
+ *                type: string
+ *              photoURL:
+ *                type: string
+ *              accountId:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: string
+ *                  email:
+ *                    type: string
+ *                  password:
+ *                    type: string
+ *                  role:
+ *                    type: string
+ *                    enum:
+ *                      - CLIENT
+ *                      - ADMIN
+ *                  status:
+ *                    type: string
+ *                    enum:
+ *                      - ACTIVE
+ *                      - INACTIVE
+ *              contracts:
+ *                type: array
+ *                items:
+ *                  type: object
+ *                  properties:
+ *                    designId:
+ *                      type: object
+ *                      properties:
+ *                        id:
+ *                          type: string
+ *                        designName:
+ *                          type: string
+ *                    contractPrice:
+ *                      type: number
+ *                    status:
+ *                      type: string
+ *                      enum:
+ *                        - CANCEL
+ *                        - PROCESSING
+ *                        - SUCCESS
+ *          ClientData:
+ *            type: object
+ *            properties:
+ *              id:
+ *                type: string
+ *              firstName:
+ *                type: string
+ *              lastName:
+ *                type: string
+ *              birthDate:
+ *                type: string
+ *              phone:
+ *                type: string
+ *              photoURL:
+ *                type: string
+ *              accountId:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: string
+ *                  email:
+ *                    type: string
+ *                  password:
+ *                    type: string
+ *                  role:
+ *                    type: string
+ *                    enum:
+ *                      - CLIENT
+ *                      - ADMIN
+ *                  status:
+ *                    type: string
+ *                    enum:
+ *                      - ACTIVE
+ *                      - INACTIVE
+ *              contracts:
+ *                type: array
+ *                items:
+ *                  type: object
+ *                  properties:
+ *                    designId:
+ *                      type: object
+ *                      properties:
+ *                        id:
+ *                          type: string
+ *                        designName:
+ *                          type: string
+ *                    contractPrice:
+ *                      type: number
+ *                    status:
+ *                      type: string
+ *                      enum:
+ *                        - CANCEL
+ *                        - PROCESSING
+ *                        - SUCCESS
+ */
+
 const ClientRouter = (app) => {
+
   router.get("/", clientService.getClients);
+
+
   router.get("/:id", clientService.getClientById);
 
   router.post("/", clientService.createClient);
