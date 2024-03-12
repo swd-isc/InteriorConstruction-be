82546046de2ae22a0e293bc31788eaaa161d6cc8
kietpt2003 <tuankiet29012003@gmail.com>
2024-03-12 20:39:52 +0700

    update swagger refund endpoint

--- src/routes/PaymentAPI.js
+++ src/routes/PaymentAPI.js
@@ -21,12 +21,12 @@ const PaymentRouter = (app) => {
      *      description: |
      *          This endpoint is used to create a payment URL.
      *          The following parameters are expected in the request:
-     *          - `bankCode`: The bank code, possible values are " ", "VNBANK", "INTCARD".
-     *          - `language`: The language, possible values are " ", "vn", "en".
+     *          - `bankCode`: The bank code, possible values are `" "`, `"VNBANK"`, `"INTCARD"`.
+     *          - `language`: The language, possible values are `" "`, `"vn"`, `"en"`.
      *          Authorization is required, and the token should be provided in the request header.
-     *          This endpoint is for Client only, Admin cannot access this endpoint
+     *          This endpoint is for `Client only`, Admin cannot access this endpoint
      *          - `amount`: Total prices
-     *          - `cart`: Array of furnitures. Ex: [{"furnitureId": "12345678","quantity": 2}]
+     *          - `cart`: Array of furnitures. Ex: `[{"furnitureId": "12345678","quantity": 2}]`
      *      requestBody:
      *          required: true
      *          content:
@@ -104,12 +104,103 @@ const PaymentRouter = (app) => {
      *                              messageError:
      *                                type: string 
      */
-    // router.post('/create_payment', verifyToken, isCurrentUser, paymentController.postPayment);
     router.post('/create_payment', verifyToken, isClient, paymentController.postPayment);
 
     router.post('/querydr', paymentController.postQuery);
 
-    router.post('/refund', paymentController.postRefund);
+    /**
+     * @swagger
+     * /api/payment/refund:
+     *  post:
+     *      security:
+     *          - bearerAuth: []
+     *      tags:
+     *          - Payments
+     *      summary: Refund payment
+     *      description: |
+     *          This endpoint is used to refund a payment.
+     *          The following parameters are expected in the request:
+     *          - `user`: Author create request
+     *          - `orderId`: OrderId is contractId
+     *          Authorization is required, and the token should be provided in the request header.
+     *          This endpoint is for `Client only`, Admin cannot access this endpoint
+     *          - `amount`: Price want to refund (remember this price must `10000 <= x <= Total price`)
+     *          - `transDate`: Date create payment (Ex: `20240312164311`) (Required)
+     *          - `transType`: Type (2 options - "02" || "03")
+     *          -       "02": Giao dịch hoàn trả toàn phần (vnp_TransactionType=02)
+     *          -       "03": Giao dịch hoàn trả một phần (vnp_TransactionType=03)
+     *      requestBody:
+     *          required: true
+     *          content:
+     *            application/json:
+     *              schema:
+     *                type: object
+     *                properties:
+     *                  user:
+     *                    type: string
+     *                  orderId:
+     *                    type: string
+     *                  amount:
+     *                    type: number
+     *                  transDate:
+     *                    type: string
+     *                  transType:
+     *                    type: string
+     *      responses:
+     *        201:
+     *          description: Ok
+     *          content:
+     *            application/json:
+     *              schema:
+     *                type: object
+     *                properties:
+     *                  status:
+     *                    type: number
+     *                  data:
+     *                    $ref: '#components/schemas/RefundData'
+     *                  message:
+     *                    type: string
+     *        400:
+     *          description: Bad request
+     *          content:
+     *            application/json:
+     *              schema:
+     *                type: object
+     *                properties:
+     *                  status:
+     *                    type: number
+     *                  data:
+     *                    type: object
+     *                  messageError:
+     *                    type: string
+     *        500:
+     *          description: Server error
+     *          content:
+     *            application/json:
+     *              schema:
+     *                type: object
+     *                properties:
+     *                  status:
+     *                    type: number
+     *                  messageError:
+     *                    type: string
+     *        401:
+     *             description: Unauthorized
+     *             content:
+     *                  application/json:
+     *                      schema:
+     *                          type: string
+     *        403:
+     *             description: Forbidden
+     *             content:
+     *                  application/json:
+     *                      schema:
+     *                          type: object
+     *                          properties:
+     *                              messageError:
+     *                                type: string 
+     */
+    router.post('/refund', verifyToken, isClient, paymentController.postRefund);
 
     router.get('/return', paymentController.returnPayment);
 
